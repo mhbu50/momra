3ff5c32442e7756fdd9ab1d640baa59451b52993
root <root@127.0.1.1mohd-io>
2021-06-14 16:16:47 +0300

    add doctyps

---
+++ momra/momra/doctype/database_object/database_object.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, momra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Database Object', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ momra/momra/doctype/service_category/service_category.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, momra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Service Category', {
+	// refresh: function(frm) {
+
+	// }
+});

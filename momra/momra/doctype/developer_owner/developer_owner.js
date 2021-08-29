d806a8190a561edf27635655e5e51655f85e1c31
frappe <frappe@ubuntu.vm>
2021-08-29 12:51:27 +0000

    add tables

---
+++ momra/momra/doctype/developer_owner/developer_owner.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, momra and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Developer Owner', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ momra/momra/doctype/operation_owner/test_operation_owner.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021, momra and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestOperationOwner(unittest.TestCase):
+	pass
